91d0d9d7082a1b53c92f2f4d42da64f89ecea5f7
Lepp <lepplogin@gmail.com>
2026-05-11 21:41:58 -0300

    exercicios em .js

---
+++ ex008.js
@@ -0,0 +1,8 @@
+console.log('Oconsole funcionou corretamente')
+
+var vel = 40.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) { //condição simples
+    console.log('Multado, está acima da velocidade permitida')
+}
+console.log('Dirija sempre com cinto de segurança')
