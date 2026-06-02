c0094d072d851640c617131607a7f725bc8c8459
Lepp <lepplogin@gmail.com>
2026-06-02 16:33:09 -0300

    Repetições em js

---
+++ ex013.js
@@ -0,0 +1,22 @@
+let c = 1
+do {
+    console.log (`passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+
+
+
+/*let c = 1
+while (c <= 5) {
+    console.log (`passo ${c}`)
+    c++
+}*/
+
+
+/*console.log ('oi')
+console.log ('oi')
+console.log ('oi')
+console.log ('oi')
+console.log ('oi')*/
