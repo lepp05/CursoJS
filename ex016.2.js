caf5ae3819c1d57755fbda70e8b76fe7b919c341
Lepp <lepplogin@gmail.com>
2026-06-09 22:04:33 -0300

    continuando em funções

---
+++ ex016.2.js
@@ -0,0 +1,14 @@
+//continuando em funções
+
+            
+function soma(n1=0,n2=0){
+    return n1 + n2 
+}
+console.log(soma(4,5))
+
+// tbm podemos colocar uam função dentro de uma variavel
+
+let p = function(x){
+    return x*2
+}
+console.log(p(5))
