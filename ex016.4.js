d576d376a2ef980ea4f8be975e9a453df46d3dac
Lepp <lepplogin@gmail.com>
2026-06-09 22:23:41 -0300

    continuando em funções

---
+++ ex016.4.js
@@ -0,0 +1,19 @@
+//RECURSIVIDADE
+
+/*
+
+5! = 5x4x3x2x1
+5! = 5 x 4!
+
+n!= n x (n-1)!
+1! = 1
+
+*/
+function fatorial(n){
+    if(n == 1){
+        return 1
+    }else{
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
